5582eedd07b3734946fe02431425c9555ba1f592
Jared Boleyn <Jared Boleyn>
2017-05-01 23:46:55 -0500

    Add project files.

---
+++ WishlistApp3/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WishlistApp3
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ WishlistApp3/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WishlistApp3.Models;
+
+namespace WishlistApp3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public wishlistDBEntities db = new wishlistDBEntities();
+
+        public ActionResult Index()
+        {
+            return View(db.persons.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegButton(string button)
+        {
+            return View("TestView");
+        }
+    }
+}
